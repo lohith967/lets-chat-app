59d48472a0cc893a6db49e7a61a874296b06fe73
lohith967 <72453060+lohith967@users.noreply.github.com>
2021-03-04 19:52:37 +0530

    Add files via upload

--- kwitter_room.js
+++ kwitter_room.js
@@ -1,13 +1,49 @@
 var firebaseConfig = {
-    apiKey: "AIzaSyAoHoR-_6Jx9LMs76aMBgjTAvIiMLVzWiY",
-    authDomain: "classtest-f7c01.firebaseapp.com",
-    projectId: "classtest-f7c01",
-    storageBucket: "classtest-f7c01.appspot.com",
-    messagingSenderId: "982166527375",
-    appId: "1:982166527375:web:5d2202df38aa32c69bfffa"
+    apiKey: "AIzaSyBi1Sb3CIjaqSFOfBeonRWqwIbNKID4RXM",
+    authDomain: "lets-chat-app-e9d2a.firebaseapp.com",
+    projectId: "lets-chat-app-e9d2a",
+    storageBucket: "lets-chat-app-e9d2a.appspot.com",
+    messagingSenderId: "63120398610",
+    appId: "1:63120398610:web:7a2023869087f8c3f8fbcb"
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
 user_name=localStorage.getItem("user_name");
 room_name=localStorage.getItem("room_name");
 document.getElementById("user_name").innerHTML="welcome "+user_name+"!";
+function addRoom()
+{
+  room_name = document.getElementById("room_name").value;
+
+  firebase.database().ref("/").child(room_name).update({
+    purpose : "adding room name"
+  });
+
+    localStorage.setItem("room_name", room_name);
+    
+    window.location = "kwitter_page.html";
+}
+
+function getData() {  firebase.database().ref("/").on('value', function(snapshot) {
+    document.getElementById("output").innerHTML = "";
+    snapshot.forEach(function(childSnapshot) {
+       childKey  = childSnapshot.key;
+       Room_names = childKey;
+       console.log("Room Name - " + Room_names);
+      row = "<div class='room_name' id="+Room_names+" onclick='redirectToRoomName(this.id)' >#"+ Room_names +"</div><hr>";
+      document.getElementById("output").innerHTML += row;
+    });
+  });
+
+}
+
+function getData() {firebase.database().ref("/").on('value',
+function(snapshot) {document.getElementById("output").innerHTML =
+"";snapshot.forEach(function(childSnapshot) {childKey =
+childSnapshot.key;
+ Room_names = childKey;
+ //Start code
+ //End code
+ });});}
+getData();
+
